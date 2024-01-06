a7d272d4a96d3b14fabc25d6bc72112838743c69
MrS-E <simeon.stix@gmx.ch>
2024-01-06 16:06:03 +0100

    setup electron app

---
+++ main.js
@@ -0,0 +1,60 @@
+const { app, BrowserWindow, screen, ipcMain } = require('electron');
+const moveWindowSmoothly = require("./js/moveWindow");
+const path = require("path");
+const robot = require('robotjs');
+
+let win;
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 800,
+        transparent: true,
+        frame: false,
+        resizable: false,
+        alwaysOnTop: true,
+        skipTaskbar: true,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            enableRemoteModule: false,
+            preload: path.join(__dirname, "./preload.js")
+        }
+    });
+
+    win.loadFile(path.join(__dirname, "index.html"));
+    win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+ipcMain.on('moveWindow', (event, args) => {
+    const {width, height} = screen.getPrimaryDisplay().size;
+    const position = win.getPosition();
+    console.log("moveWindow", args);
+    if(position[0]+args[0] < 0 || position[0]+args[0] > width-win.getSize()[0]) {
+        args[0] = 0;
+    }
+    if(position[1]+args[1] < 0 || position[1]+args[1] > width-win.getSize()[1]){
+        args[1] = 0;
+    }
+    const duration = Math.round(Math.sqrt(Math.pow(args[0], 2) + Math.pow(args[1], 2))/148*1000);
+    console.log("duration", duration);
+    moveWindowSmoothly(win, position[0]+args[0], position[1]+args[1],duration);
+
+});
+
+setTimeout(() => {
+    robot.moveMouse(100, 100);
+}, 2000);
