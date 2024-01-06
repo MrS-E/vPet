45d11b1b1af0007cc3c0c07f89815154d4034fc3
MrS-E <simeon.stix@gmx.ch>
2024-01-06 18:02:30 +0100

    small changes

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const { app, BrowserWindow, screen, ipcMain } = require('electron');
-const moveWindowSmoothly = require("./js/moveWindow");
+const {moveWindowSmoothly, moveCursorSmoothly} = require("./js/movement");
 const path = require("path");
-const robot = require('robotjs');
+const robot = require("@jitsi/robotjs");
 
 let win;
 function createWindow() {
     win = new BrowserWindow({
-        width: 800,
-        height: 800,
+        width: 200,
+        height: 200,
         transparent: true,
         frame: false,
         resizable: false,
@@ -22,7 +22,7 @@ function createWindow() {
     });
 
     win.loadFile(path.join(__dirname, "index.html"));
-    win.webContents.openDevTools();
+    //win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
@@ -42,7 +42,19 @@ app.on('activate', () => {
 ipcMain.on('moveWindow', (event, args) => {
     const {width, height} = screen.getPrimaryDisplay().size;
     const position = win.getPosition();
-    console.log("moveWindow", args);
+    if(position[0]+args[0] < 0 || position[0]+args[0] > width-win.getSize()[0]) {
+        args[0] = 0;
+    }
+    if(position[1]+args[1] < 0 || position[1]+args[1] > width-win.getSize()[1]){
+        args[1] = 0;
+    }
+    const duration = args[2];
+    moveWindowSmoothly(win, position[0]+args[0], position[1]+args[1],duration);
+});
+
+ipcMain.on('steelCursor', (event, args) => {
+    const {width, height} = screen.getPrimaryDisplay().size;
+    const position = win.getPosition();
     if(position[0]+args[0] < 0 || position[0]+args[0] > width-win.getSize()[0]) {
         args[0] = 0;
     }
@@ -50,11 +62,8 @@ ipcMain.on('moveWindow', (event, args) => {
         args[1] = 0;
     }
     const duration = Math.round(Math.sqrt(Math.pow(args[0], 2) + Math.pow(args[1], 2))/148*1000);
-    console.log("duration", duration);
     moveWindowSmoothly(win, position[0]+args[0], position[1]+args[1],duration);
-
+    moveCursorSmoothly(robot, position[0]+args[0], position[1]+args[1], duration);
 });
 
-setTimeout(() => {
-    robot.moveMouse(100, 100);
-}, 2000);
+
